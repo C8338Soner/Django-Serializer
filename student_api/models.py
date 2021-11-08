62e4534c777990df4242da428bf9ffa195a0e799
C8338Soner <sonerpeker84@gmail.com>
2021-11-08 04:12:14 +0300

    SerializerModel

--- student_api/models.py
+++ student_api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+
+class Path(models.Model):
+ path_name= models.CharField(max_length=30)
+ 
+ def __str__(self):
+  return self.path_name
+
 class Student(models.Model):
+ path = models.ForeignKey(Path, related_name = 'students', on_delete=models.CASCADE)
  first_name = models.CharField(max_length=100)
  last_name = models.CharField(max_length=100)
  number = models.IntegerField(blank=True, null=True)
--- student_api/serializers.py
+++ student_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Path
 
 #Bu yöntem zahmetli bunun yerine .models fields kullanılacak
 # class Student(serializers.Model):
@@ -7,10 +7,24 @@ from .models import Student
 #  last_name = serializers.CharField(max_length=100)
 #  number = serializers.IntegerField(required=False)
 
+class PathSerializer(serializers.ModelSerializer):
+ # students = serializers.PrimaryKeyRelatedField(read_only=True, many = True)
+ students =  serializers.HyperlinkedIdentityField(view_name='detail', format='html', many=True, read_only=True)
+ class Meta:
+  model = Path
+  fields = '__all__'
+
 class StudentSerializer(serializers.ModelSerializer):
+ path = serializers.StringRelatedField()
+ path_id = serializers.IntegerField()
+ paths = serializers.SerializerMethodField()
  class Meta:
   model = Student
-  # fields = ('first_name', 'last_name', 'number')
+  fields = ('first_name', 'last_name', 'number', 'path', 'path_id', "paths")
   # exclude = ('number',)
-  fields = '__all__'
+  #fields = '__all__'
  
+ def get_paths(self,obj):
+  paths_data = Path.objects.all()
+  serializer = PathSerializer(paths_data, many = True)
+  
